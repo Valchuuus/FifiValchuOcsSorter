d08dd43cf1ed458a7054902da5c5a0145720504a
Valchuuus <valehetaliana123@gmail.com>
2025-08-21 18:04:37 -0400

    Update data.js

--- src/js/data.js
+++ src/js/data.js
@@ -15,7 +15,7 @@ const dataSet = {};
  * 
  * @example '2018-02-20'
 */
-let dataSetVersion = '2025-08-15';
+let dataSetVersion = '2025-08-21';
 /**
  * Image root, will be appended to the start of every image URL.
  */
